e751391ed6ee313df512c49792943db698d69370
Micael Melo <micael.melo@cashway.io>
2023-06-21 20:40:23 -0300

    ajusts cores background

--- components/shared/MainComponent/index.tsx
+++ components/shared/MainComponent/index.tsx
@@ -4,7 +4,7 @@ import Header from '../Header/StorefrontHeader'
 
 const MainComponent: React.FC = ({children}) => {
   return ( 
-    <div class="d-flex flex-column stick-footer-wrapper">
+    <div className="d-flex flex-column stick-footer-wrapper">
       <Header />
       <div className="container flex-fill">
         {children}
